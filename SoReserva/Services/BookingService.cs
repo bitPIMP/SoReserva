7f6dcf26d0e72d1299535f18108c8379539ffbbf
Marcio Junqueira de Rezende <devpromarc@gmail.com>
2022-05-26 16:49:19 -0300

    Funciona Mas analisa só as datas sem analisar o veículo atrelado a essas datas.

--- SoReserva/Services/BookingService.cs
+++ SoReserva/Services/BookingService.cs
@@ -37,7 +37,9 @@ namespace SoReserva.Services
                                         .ToList()
                                         .FirstOrDefault();
 
-            string CriticalReturningDateString = CriticalReturningDate.ToString();// aqui está chegando só o Returning Date pq eu fiz o .Select(x => x.Returning)
+            //string CriticalReturningDateString = CriticalReturningDate.ToString();// aqui está chegando só o Returning Date pq eu fiz o .Select(x => x.Returning)
+            #region Anotações 1
+
             //string CriticalReturningDateString = CriticalReturningDate.Returning.ToString();
             //
 
@@ -49,10 +51,17 @@ namespace SoReserva.Services
             // 1) agrupa as datas de devolução que são menores que a data de início da locação solicitada
             // 2) Ordena do maior para o menor: a maior data é a que mais se aproxima do início da locação solicitada
             // 3) Isolo a data de devolução que ocorre antes do início da locação solicitada
+            #endregion
+            var CriticalBeginningDate = _context.Booking.Where(x => x.Beginning > d_devolucao)
+                                        .OrderBy(x => x.Beginning)
+                                        .Select(x=> x.Beginning)
+                                        .ToList()
+                                        .FirstOrDefault();
 
-            var CriticalBeginningDate = _context.Booking.Where(x => x.Beginning > d_devolucao).OrderBy(x => x.Beginning).Select(x=> x.Beginning).FirstOrDefault();
-            string CriticalBeginningDateString = CriticalBeginningDate.ToString();
 
+            //string CriticalBeginningDateString = CriticalBeginningDate.ToString();
+
+            #region Anotações 2
             //string CriticalBeginningDateString = CriticalBeginningDate.Beginning?.ToString(); //Operator ? cannot applied to operand of type DateTime 
 
 
@@ -61,16 +70,23 @@ namespace SoReserva.Services
             // Vejo o tamanho da Lista. Se ela for vazia então estou criando a primeira reserva
             /* if Lista.Count >0 return true
              else faz  DateTime.Compare(Returning, d_inicio) && 
-             
-             
-             
-             
              */
+            #endregion
+            // se não existe nenhuma reserva pode fazer qualquer reserva
+            // deixa a primeira reserva ser criada porque não existe nenhuma!
+            // o DateTime.MinValue mata a necessidade de usar o string pra detectar nulos. 
+            // o DateTime.MinValue, nesse contexto, funciona como o null
+            // Se não tiver nenhuma reserva pode fazer. OK
+            if ((CriticalReturningDate == DateTime.MinValue && CriticalBeginningDate == DateTime.MinValue)) return true; // o null == a primeira data possível no DateTime?
 
-            if (!(String.IsNullOrEmpty(CriticalReturningDateString) || String.IsNullOrEmpty(CriticalBeginningDateString))) return true; // o null == a primeira data possível no DateTime?
-            // deixa a primeira reserva ser criada porque não existe nenhuma!                        
 
-            if ((CriticalReturningDate == d_inicio) || (CriticalBeginningDate == d_devolucao)) return false;
+            //Se o dia de início for maior que a última devolução + 1 dia e a data de devolução é menor que o próxima dia de início -1
+            if ((d_inicio >= CriticalReturningDate.AddDays(1)) && (d_devolucao <= CriticalBeginningDate.AddDays(-1))) return true;
+            // parece que esse comando está resolvendo as restrições de +-1 dia mesmo quando uma dessas variáveis coincide com o dia de hoje.  
+
+            if ((DateTime.Today >= CriticalReturningDate.AddDays(1)) && (d_devolucao <= CriticalBeginningDate.AddDays(-1))) return true;
+
+            //if ((CriticalReturningDate == d_inicio) || (CriticalBeginningDate == d_devolucao)) return false;
 
             return false;
 
