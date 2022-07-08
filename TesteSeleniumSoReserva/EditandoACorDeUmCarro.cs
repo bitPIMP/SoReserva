83e97947af0721ce471cd7341c1099f78f71b4a5
Marcio Junqueira de Rezende <devpromarc@gmail.com>
2022-07-08 16:09:17 -0300

    Remoção de referências obsoletas e organização do código do teste

--- TesteSeleniumSoReserva/EditandoACorDeUmCarro.cs
+++ TesteSeleniumSoReserva/EditandoACorDeUmCarro.cs
@@ -1,11 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
 using System.IO;
 using System.Reflection;
 using Xunit;
-using SoReserva.Services;
-using SoReserva.Data;
 
 
 namespace TesteSeleniumSoReserva
@@ -17,15 +14,13 @@ namespace TesteSeleniumSoReserva
         public void TestandoInterfaceCarregaPaginaHomeEVerificaTitulo() 
         {
             //Arrange
-            
-            // Descubro por reflection a localização do ChromeDriver
             IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
 
             //Act
             driver.Navigate().GoToUrl("https://localhost:44331/");
 
             //Assert
-            Assert.Contains("Home Page - SoReserva", driver.Title);           
+            Assert.Contains("Home Page - SoReserva", driver.Title);
 
         }       
     }
